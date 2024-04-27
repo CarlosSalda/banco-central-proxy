75546cde44b92ee5d06cd45fda7f1d1e0c941da4
Charly <carlos@dango.digital>
2024-04-27 02:02:53 -0300

    Update Code & packages

--- server.ts
+++ server.ts
@@ -1,31 +1,42 @@
-import express from "express";
-import { createProxyMiddleware, Options } from "http-proxy-middleware";
+import express, { Request, Response } from "express";
 import cors from "cors";
 
 const PORT = process.env.PORT ?? 3000;
-const API_SERVICE_URL = "https://api.estadisticasbcra.com";
-
 const app = express();
-
-interface ProxyOptionsExtended extends Options {
-  onProxyReq?: (
-    proxyReq: any,
-    req: express.Request,
-    res: express.Response
-  ) => void;
-}
-
-const proxyOptions: ProxyOptionsExtended = {
-  target: API_SERVICE_URL,
-  changeOrigin: true,
-  pathRewrite: { "^/api": "" },
-  onProxyReq: (proxyReq, req, res) => {
-    proxyReq.setHeader("X-Added", "proxy-header");
-  },
-};
-
 app.use(cors());
-app.use("/api", createProxyMiddleware(proxyOptions));
+
+app.get("/", (req: Request, res: Response) => {
+  const htmlResponse = `
+    <html>
+      <head>
+        <title>BCRA Proxy CORS</title>
+      </head>
+      <body>
+        <h1>BCRA Proxy CORS</h1>
+        <p>Se trata de un servidor proxy que permite acceder a la API del Banco Central de Argentina sin encontrarse con conflictos de CORS.</p>
+      </body>
+    </html>
+  `;
+  res.send(htmlResponse);
+});
+
+app.get("/:url(*)", (req: Request, res: Response) => {
+  const apiUrl = `https://api.estadisticasbcra.com/${req.params.url}`;
+  const headers = {
+    Authorization: req.get("Authorization") ?? "",
+    "Content-Type": "application/json",
+  };
+
+  fetch(apiUrl, { headers, mode: "cors" })
+    .then((response) => response.json())
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.sendStatus(500);
+    });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
